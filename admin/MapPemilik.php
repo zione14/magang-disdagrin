f00523755caeec32871fba619788aa0cc9907a2a
zione14 <aryadhivahandy1505@gmail.com>
2024-02-20 09:56:34 +0700

    Update MapPemilik.php

--- admin/MapPemilik.php
+++ admin/MapPemilik.php
@@ -8,7 +8,9 @@ $NamaPasar = htmlspecialchars(base64_decode($_GET['n']));
 $sql_lapakpr = "SELECT l.IDLapak, l.BlokLapak, l.NomorLapak, l.Keterangan, l.Retribusi, l.KodePasar
   FROM lapakpasar l 
   WHERE l.KodePasar='$KodePasar' AND l.IDLapak='$IDLapak' ORDER BY IDLapak ASC";
-  $res_lapakpr = $koneksi->query($sql_lapakpr);
+  $oke = $koneksi->prepare($sql_lapakpr);
+  $oke->execute();
+  $res_lapakpr = $oke->get_result();
   $row_lapakpr = $res_lapakpr->fetch_assoc();
 
 ?>
@@ -107,7 +109,9 @@ $sql_lapakpr = "SELECT l.IDLapak, l.BlokLapak, l.NomorLapak, l.Keterangan, l.Ret
                                         
                                         
                                         $sql .=" GROUP BY d.IDPerson order by d.NamaPerson";
-                                        $result = mysqli_query($koneksi,$sql);  
+                                        $oke1 = $koneksi->prepare($sql);
+                                        $oke1->execute();
+                                        $result = $oke1->get_result();  
                                         
                                         //pagination config start
                                         $rpp = 20; // jumlah record per halaman
